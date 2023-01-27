f6fda190889fb92caa7558d18f494a0b9f5d1937
rose <rosefor2ne@gmail.com>
2023-01-27 17:55:59 +0900

    타임 알리아스 -> 인터페이스로 변경, 리드온리 적용

--- app.ts
+++ app.ts
@@ -1,29 +1,29 @@
 
 
-type Store = {
+interface Store {
   currentPage: number;
   feeds: NewsFeed[];
 }
 
-type News = { //인터섹션, 타입알리아스
-  id: number;
-  time_ago: string;
-  title: string;
-  url: string;
-  user: string;
-  content: string;
+interface News { //인터섹션, 타입알리아스
+  readonly id: number;
+  readonly time_ago: string;
+  readonly title: string;
+  readonly url: string;
+  readonly user: string;
+  readonly content: string;
 }
-type NewsFeed = News & {
-  comments_count: number;
-  points: number;
+interface NewsFeed extends News {
+  readonly comments_count: number;
+  readonly points: number;
   read?: boolean; //옵셔널
 }
-type NewsDeail = News & {
-  comments: NewsComment[];
+interface NewsDeail extends News {
+  readonly comments: NewsComment[];
 }
-type NewsComment = News & {
-  comments: NewsComment[];
-  level: number;
+interface NewsComment extends News {
+  readonly comments: NewsComment[];
+  readonly level: number;
 }
 
 const container: HTMLElement | null = document.getElementById('root');
